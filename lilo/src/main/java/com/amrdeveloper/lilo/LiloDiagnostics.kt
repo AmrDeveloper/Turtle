cd0b79a856a379951f45cb855dd5fbda17b1d746
AmrDeveloper <amr96@programmer.net>
2022-06-17 22:49:25 +0200

    Implement Diagnostics that can save errors and warns with positions

---
+++ lilo/src/main/java/com/amrdeveloper/lilo/LiloDiagnostics.kt
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 AmrDeveloper (Amr Hesham)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.amrdeveloper.lilo
+
+class LiloDiagnostics {
+
+    private val errorDiagnostics = mutableListOf<Diagnostics>()
+    private val warnsDiagnostics = mutableListOf<Diagnostics>()
+
+    fun reportError(position: TokenPosition, message : String) {
+        errorDiagnostics.add(Diagnostics(position, message, DiagnosticType.ERROR))
+    }
+
+    fun reportWarn(position: TokenPosition, message : String) {
+        warnsDiagnostics.add(Diagnostics(position, message, DiagnosticType.WARN))
+    }
+
+    fun errorDiagnostics() : List<Diagnostics> = errorDiagnostics
+
+    fun warnsDiagnostics() : List<Diagnostics> = warnsDiagnostics
+
+    fun clearErrors() {
+        errorDiagnostics.clear()
+    }
+
+    fun clearWarns() {
+        warnsDiagnostics.clear()
+    }
+
+    fun errorNumber() : Int = errorDiagnostics.size
+
+    fun warnsNumber() : Int = warnsDiagnostics.size
+}

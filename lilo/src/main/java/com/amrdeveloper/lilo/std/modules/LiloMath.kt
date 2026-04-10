1f74cd80ead3f1d241e023437d3c435efdc63544
AmrDeveloper <amr96@programmer.net>
2026-04-10 21:48:15 +0200

    Implement `math.pi` and `math.tau` builtin constant

--- lilo/src/main/java/com/amrdeveloper/lilo/std/modules/LiloMath.kt
+++ lilo/src/main/java/com/amrdeveloper/lilo/std/modules/LiloMath.kt
@@ -5,7 +5,11 @@ import com.amrdeveloper.lilo.`object`.LiloModule
 
 private const val MODULE_NAME = "math"
 
+private const val TAU = 6.283185307179586.toFloat()
+
 val liloMathModule = LiloModule(name = MODULE_NAME).also {
     it.setAttr(name = "inf", value = LiloFloat(value = Float.POSITIVE_INFINITY))
     it.setAttr(name = "nan", value = LiloFloat(value = Float.NaN))
+    it.setAttr(name = "pi", value = LiloFloat(value = Math.PI.toFloat()))
+    it.setAttr(name = "tau", value = LiloFloat(value = TAU))
 }

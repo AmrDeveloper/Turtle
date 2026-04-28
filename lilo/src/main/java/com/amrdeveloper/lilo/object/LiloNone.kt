f2f880d280b3f75208720947f0b685b056687e02
AmrDeveloper <amr96@programmer.net>
2026-04-28 22:22:47 +0200

    Implement `__bool__` function for NoneType

--- lilo/src/main/java/com/amrdeveloper/lilo/object/LiloNone.kt
+++ lilo/src/main/java/com/amrdeveloper/lilo/object/LiloNone.kt
@@ -1,7 +1,24 @@
 package com.amrdeveloper.lilo.`object`
 
+import com.amrdeveloper.lilo.common.LiloMagicMethod
+import com.amrdeveloper.lilo.common.LiloResult
+import com.amrdeveloper.lilo.runtime.LiloCallable
+import com.amrdeveloper.lilo.runtime.LiloInterpreter
+import com.amrdeveloper.lilo.type.liloMethodType
 import com.amrdeveloper.lilo.type.liloNoneType
 
 data class LiloNone(private val unit: Unit = Unit) : LiloObject(liloNoneType) {
+    init {
+        setAttr(name = LiloMagicMethod.BOOL, value = NoneBool)
+    }
     override fun toString() = "None"
+}
+
+private object NoneBool : LiloObject(liloMethodType), LiloCallable {
+    override fun invoke(
+        interpreter: LiloInterpreter,
+        args: List<LiloObject>
+    ): LiloResult<LiloObject> {
+        return LiloResult.Success(data = LiloBool(value = false))
+    }
 }

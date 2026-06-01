9fc87aef788ab45042f03611417153347bf70e62
AmrDeveloper <amr96@programmer.net>
2026-06-01 21:32:14 +0200

    Implement `GT` for Float

--- lilo/src/main/java/com/amrdeveloper/lilo/objects/LiloFloat.kt
+++ lilo/src/main/java/com/amrdeveloper/lilo/objects/LiloFloat.kt
@@ -15,9 +15,14 @@ val liloFloatType = LiloType(name = "float", bases = listOf(LiloBaseType.LILO_OB
     it.setAttr(name = LiloMagicMethod.MUL, value = FloatMul)
     it.setAttr(name = LiloMagicMethod.DIV, value = FloatDiv)
 
+    // Comparisons
+    it.setAttr(name = LiloMagicMethod.GT, value = FloatGT)
+
     // Unary
     it.setAttr(name = LiloMagicMethod.POS, value = FloatPos)
     it.setAttr(name = LiloMagicMethod.NEG, value = FloatNeg)
+
+    it.setAttr(name = LiloMagicMethod.BOOL, value = FloatBool)
 }
 
 private object FloatAdd : LiloObject(liloFunctionType), LiloCallable {
@@ -92,6 +97,22 @@ private object FloatDiv : LiloObject(liloFunctionType), LiloCallable {
     }
 }
 
+private object FloatGT : LiloObject(liloFunctionType), LiloCallable {
+    override fun invoke(
+        interpreter: LiloInterpreter,
+        args: List<LiloObject>
+    ): LiloResult<LiloObject> {
+        val lhs = args[0] as LiloFloat
+        val rhsValue = when (val rhs = args[1]) {
+            is LiloFloat -> rhs.value
+            is LiloInt -> rhs.value.toDouble()
+            else -> 0.0
+        }
+        return LiloResult.Success(data = LiloBool(value = lhs.value > rhsValue))
+    }
+}
+
+
 private object FloatPos : LiloObject(liloFunctionType), LiloCallable {
     override fun invoke(
         interpreter: LiloInterpreter,
@@ -118,26 +139,20 @@ private object FloatNeg : LiloObject(liloFunctionType), LiloCallable {
     }
 }
 
-
-data class LiloFloat(val value: Double) : LiloObject(liloFloatType) {
-
-    init {
-        setAttr(name = LiloMagicMethod.BOOL, value = FloatBool)
-    }
-
-    override fun toString(): String {
-        if (value == Double.POSITIVE_INFINITY) return "inf"
-        if (value.isNaN()) return "nan"
-        return value.toString()
-    }
-}
-
-private object FloatBool : LiloObject(liloMethodType), LiloCallable {
+private object FloatBool : LiloObject(liloFloatType), LiloCallable {
     override fun invoke(
         interpreter: LiloInterpreter,
         args: List<LiloObject>
     ): LiloResult<LiloObject> {
         val self = args[0] as LiloFloat
         return LiloResult.Success(data = LiloBool(value = self.value != 0.0))
+    }
+}
+
+data class LiloFloat(val value: Double) : LiloObject(liloFloatType) {
+    override fun toString(): String {
+        if (value == Double.POSITIVE_INFINITY) return "inf"
+        if (value.isNaN()) return "nan"
+        return value.toString()
     }
 }
